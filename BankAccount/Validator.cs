4e650c8ed4bae79030bc9c9517a103e2a643fd95
Jessie <jessietajalle08@gmail.com>
2024-08-13 19:24:12 -0700

    Added static method test

---
+++ BankAccount/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public static class Validator
+    {
+        public static bool IsWithinRange(double minValue, double maxValue, double valueToTest)
+        {
+            if (valueToTest >= minValue && valueToTest <= maxValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
