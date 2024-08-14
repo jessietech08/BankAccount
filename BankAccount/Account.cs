62118498ac5a24291f00061b9283f52912c4413b
Jessie <jessietajalle08@gmail.com>
2024-08-13 19:17:13 -0700

    Implemented owner property tests

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -11,6 +11,8 @@ namespace BankAccount
     /// </summary>
     public class Account
     {
+        private string owner;
+
         /// <summary>
         /// Creates account with a specific owner and a balance of 0
         /// </summary>
@@ -23,7 +25,61 @@ namespace BankAccount
         /// <summary>
         /// The account holders full name (first and last)
         /// </summary>
-        public string Owner { get; set; }
+        public string Owner 
+        {
+            get { return owner; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException($"{nameof(Owner)} cannot be null");
+                }
+
+                if (value.Trim() == string.Empty)
+                {
+                    throw new ArgumentException($"{nameof(Owner)} must have some text");
+                }
+
+                if (IsOwnerNameValid(value))
+                {
+                    owner = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"{nameof(Owner)} can be up to 20 characters, A-Z/spaces only");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if owner name is less than or equal to 20 characters A-Z
+        /// and whitespace characters are allowed
+        /// </summary>
+        /// <returns>true or false</returns>
+        private bool IsOwnerNameValid(string ownerName)
+        {
+            char[] validCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
+                                    , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
+                                    , 'u', 'v', 'w', 'x', 'y', 'z'};
+
+            const int MaxLengthOwnerName = 20;
+
+            if (ownerName.Length > MaxLengthOwnerName)
+            {
+                return false;
+            }
+            ownerName = ownerName.ToLower(); // only need to compare to one casing
+
+            foreach(char letter in ownerName)
+            {
+                if (letter != ' ' && !validCharacters.Contains(letter))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
         /// <summary>
         /// The amount of money currently in the account
--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -24,6 +24,7 @@ namespace BankAccount.Tests
         [DataRow(.01)]
         [DataRow(1.99)]
         [DataRow(9_999.99)]
+        [TestCategory("Deposit")]
         public void Deposit_APositiveAmount_AddToBalance(double depositAmount)
         {
             acc.Deposit(depositAmount);
@@ -32,6 +33,7 @@ namespace BankAccount.Tests
         }
 
         [TestMethod] // shortcut: testm
+        [TestCategory("Deposit")]
         public void Deposit_APositiveAmount_ReturnsUpdatedBalance()
         {
             // AAA - Arrange Act Assert
@@ -49,6 +51,7 @@ namespace BankAccount.Tests
         [TestMethod]
         [DataRow(-1)]
         [DataRow(0)]
+        [TestCategory("Deposit")]
         public void Deposit_ZeroOrLess_ThrowsArgumentException(double invalidDepositAmount)
         {
             // Arrange
@@ -61,6 +64,7 @@ namespace BankAccount.Tests
 
         
         [TestMethod]
+        [TestCategory("Withdraw")]
         public void Withdraw_PositiveAmount_DecreasesBalance()
         {
             //Arrange
@@ -81,6 +85,7 @@ namespace BankAccount.Tests
         [TestMethod]
         [DataRow(100, 50)]
         [DataRow(100, .99)]
+        [TestCategory("Withdraw")]
         public void Withdrawal_PositiveAmount_ReturnsUpdatedBalance(double initialDeposit, double withdrawalAmount)
         {
             //Arrange
@@ -98,17 +103,51 @@ namespace BankAccount.Tests
         [DataRow(0)]
         [DataRow(-.01)]
         [DataRow(-1000)]
+        [TestCategory("Withdraw")]
         public void Withdraw_ZeroOrLess_ThrowsArgumentOutOfRangeException(double withdrawalAmount)
         {
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => acc.WithDraw(withdrawalAmount));
         }
 
         [TestMethod]
+        [TestCategory("Withdraw")]
         public void Withdraw_MoreThanAvailableBalance_ThrowsArgumentException()
         {
             double withdrawalAmount = 1000;
 
             Assert.ThrowsException<ArgumentException>(() => acc.WithDraw(withdrawalAmount));
+        }
+
+        [TestMethod]
+        public void Owner_SetAsNull_ThrowsArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => acc.Owner = null);
+        }
+
+        [TestMethod]
+        public void Owner_SetAsWhiteSpaceOrEmptyString_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = String.Empty);
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = "    ");
+        }
+
+        [TestMethod]
+        [DataRow("Jessie")]
+        [DataRow("Jessie S")]
+        [DataRow("Jessie Jean Sumandig")]
+        public void Owner_SetAsUpTo20Characters_SetsSuccessfully(string ownerName)
+        {
+            acc.Owner = ownerName;
+            Assert.AreEqual(ownerName, acc.Owner);
+        }
+
+        [TestMethod]
+        [DataRow("Jessie 2nd")]
+        [DataRow("Jessie Jean Sumandigs")]
+        [DataRow("#$%$")]
+        public void Owner_InvalidOwnerName_ThrowsArgumentException(string ownerName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = ownerName);
         }
     }
 }
